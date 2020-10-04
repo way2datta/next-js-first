89b73507dbee97a60da65a5baaacaf8884c8d37f
Dattatray Kale <way2datta@gmail.com>
2020-10-04 12:32:09 +0530

    .

--- pages/BucketListing.js
+++ pages/BucketListing.js
@@ -1,25 +1,21 @@
 import Link from 'next/link'
-//import './BucketListing.scss';
 import Table from 'react-bootstrap/Table'
 
 export function BucketListing({ buckets }) {
-  const listItems = buckets.map((x) =>
-    <tr key={x.Name}><td><Link  href={`/bucket/${x.Name}`}>{x.Name}</Link></td>
-    <td>{new Date(x.CreationDate).toLocaleString(undefined, {timeZone: 'Asia/Kolkata'})}</td> </tr>
+  const rows = buckets.map((x) =>
+    <tr key={x.Name}><td><Link href={`/bucket/${x.Name}`}>{x.Name}</Link></td>
+      <td>{new Date(x.CreationDate).toLocaleString(undefined, { timeZone: 'Asia/Kolkata' })}</td> </tr>
   );
 
-  // return <ul> {listItems} </ul>;
   return <Table striped bordered hover responsive >
     <thead>
-        <tr>
-            <th>Bucket Name</th>
-            <th>Date Created</th>
-        </tr>
+      <tr>
+        <th>Bucket Name</th>
+        <th>Date Created</th>
+      </tr>
     </thead>
     <tbody>
-      
-            {listItems}
-      
+      {rows}
     </tbody>
-</Table>
+  </Table>
 }
